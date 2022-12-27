d279b13f3936e3ff37b04855ec79ec0a6d04ed6a
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-27 15:37:56 -0300

    Added scaffold widget in maim.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Home()));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        title: const Text('IMC Calculator'),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+      ),
+      body: Container(),
+    );
+  }
+}
